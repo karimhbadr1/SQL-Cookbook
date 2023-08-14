87896194ab1f23f0aa3c9d831d2d110e8506fdae
Karim Badr <karimhbadr@gmail.com>
2023-08-14 15:41:41 +0100

    Set up skeleton for Chapter 2

---
+++ Chapter02.sql
@@ -0,0 +1,13 @@
+# Chapter 2: Sorting Query Results
+
+# 2.1: Returning Query Results in a Specified Order
+
+# 2.2: Sorting by Multiple Fields
+
+# 2.3: Sorting by Substrings
+
+# 2.4: Sorting Mixed Alphanumeric Data
+
+# 2.5: Dealing with Nulls When Sorting
+
+# 2.6: Sorting on Data-Dependent Key
